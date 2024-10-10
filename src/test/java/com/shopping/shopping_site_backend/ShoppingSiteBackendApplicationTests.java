b04951713046331a922d57a605754cbe1a2c6598
chuenningyeh <chuenningyeh@gmail.com>
2024-10-10 17:08:02 +0800

    first commit

---
+++ src/test/java/com/shopping/shopping_site_backend/ShoppingSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.shopping_site_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
