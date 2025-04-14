a16a6e17663a40dd67e2f1b928f15dce1e07b6ad
chi252525 <chuenningyeh@gmail.com>
2025-04-14 16:06:57 +0800

    update

--- src/main/java/com/shopping/shopping_site_backend/infra/dataprovider/entity/shopper/GoogleUser.java
+++ src/main/java/com/shopping/shopping_site_backend/infra/dataprovider/entity/shopper/GoogleUser.java
@@ -3,6 +3,10 @@ package com.shopping.shopping_site_backend.infra.dataprovider.entity.shopper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.shopping.shopping_site_backend.infra.dataprovider.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +21,11 @@ import org.hibernate.annotations.DynamicUpdate;
 @NoArgsConstructor
 @DynamicUpdate
 @Entity
+@Table(name = "google_user")
 public class GoogleUser extends BaseEntity {
+  /** 唯一序號 */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @JsonProperty("id")
   private Long id;
 
